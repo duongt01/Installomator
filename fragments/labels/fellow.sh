711850f2b3dda280f61565bd216fe2e4dd170285
duongt01 <92530753+duongt01@users.noreply.github.com>
2022-04-15 16:37:39 -0500

    Create fellow.sh

---
+++ fragments/labels/fellow.sh
@@ -0,0 +1,7 @@
+fellow)
+    name="Fellow"
+    type="dmg"
+    downloadURL="https://cdn.fellow.app/desktop/1.1.0/darwin/stable/universal/Fellow-1.1.0-universal.dmg"
+    appNewVersion=""
+    expectedTeamID="2NF46HY8D8"
+    ;;
